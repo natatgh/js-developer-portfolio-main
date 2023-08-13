879e5daf2d0a886c1ea763086a5de161a042ef4b
Natã Alexandre Da Silva <86895720+natatgh@users.noreply.github.com>
2023-08-13 16:36:12 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/natatgh/js-developer-portfolio-main/main/data/profile.json?token=GHSAT0AAAAAACFV36TU4NEU574RDHSOZ46OZGZGCGA';
+    const url = 'https://raw.githubusercontent.com/natatgh/js-developer-portfolio-main/main/data/profile.json?token=GHSAT0AAAAAACFV36TUBL3CRNNCGS4NRNRQZGZGDMQ';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
