1551e55c7067c15522c621a613a347c694b5d9b0
natatgh <natatgh@gmail.com>
2023-08-13 16:02:28 -0300

    Commit v2
    Commit v2

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/natatgh/js-developer-portfolio-main/main/data/profile.json?token=GHSAT0AAAAAACFV36TUUFC6EDEM4MEIFPJWZGZE7KA';
+    const url = 'https://raw.githubusercontent.com/natatgh/js-developer-portfolio-main/main/data/profile.json?token=GHSAT0AAAAAACFV36TU5KOTKDACGWQTUNZEZGZFCNQ';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
